6c6d0271493990272bd2ec320be638df43ff2ab8
ahmedmustahid <einsteinium25@gmail.com>
2021-08-25 12:15:23 +0900

    create new build ton invokeHTTP

---
+++ ApiGatewayApp/ApiGatewayApp/ApiGatewayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiGatewayAppApp.swift
+//  ApiGatewayApp
+//
+//  Created by Ahmed on 2021/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ApiGatewayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApiGatewayApp/Pods/Target Support Files/Pods-ApiGatewayApp/Pods-ApiGatewayApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ApiGatewayApp : NSObject
+@end
+@implementation PodsDummy_Pods_ApiGatewayApp
+@end
